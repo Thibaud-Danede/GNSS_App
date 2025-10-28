a93e37fe79d710dc8db0b70c2941f3002a99deb1
gperbost00 <georges.perbost@etu.estia.fr>
2025-10-28 17:08:48 +0100

    Feat: commit initial du projet

---
+++ Geo/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geo
+{
+    public partial class Form1 : Form
+    {
+        private Thread readFileThread;
+        private string filePath = "trace.txt"; // Remplacez par le chemin de votre fichier
+        private List<GGA> listeGGA;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.listeGGA = new List<GGA>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Démarrer le thread pour lire le fichier lorsque le formulaire est chargé
+            readFileThread = new Thread(ReadFile);
+            readFileThread.Start();
+        }
+
+        private void ReadFile()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        // Utiliser Invoke pour mettre à jour l'interface utilisateur depuis un thread différent
+                        this.Invoke(new Action(() =>
+                        {
+                            //Si la trame est une GGA, je créé un objet GGA
+                            // que je remplis avec les infos de la trame
+                            Console.WriteLine(line);
+                            if (line.StartsWith("$GPGGA"))
+                            {
+                                GGA pointGGA = new GGA(line);
+                                //Je range cet objet dans la liste des GGA
+                                listeGGA.Add(pointGGA);
+
+                                //Je met à jour les champs lat, long et alt dans l'interface
+                                Console.WriteLine("Latitude : " + pointGGA.North.ToString());
+                                Console.WriteLine("Longitude : " + pointGGA.West.ToString());
+                                Console.WriteLine("Altitude : " + pointGGA.Altitude.ToString());
+                            }
+
+                            //et je fais ce que je veux avec !
+                        }));
+
+                        // Ajouter un délai pour simuler la lecture ligne par ligne (facultatif)
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la lecture du fichier : " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Geo/GGA.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geo
+{
+    class GGA
+    {
+        private DateTime _date;
+        private double _north;
+        private double _west;
+        private int _typeLocation;
+        private int _nbSat;
+        private double _altitude;
+        private string _nextData;//on l'initialise pas, la flemme
+
+        public GGA(string trameGGA)
+        {
+            //va découper une trame au format
+            //$GPGGA,153709.00,4326.7373771,N,00133.0838793,W,1,21,1.3,43.2222,M,50
+
+            string[] infoGGA = trameGGA.Split(',');
+            setDateFromGGA(infoGGA[1]);
+
+            try
+            {
+                this._north = Double.Parse(infoGGA[2].Replace('.',','))/100;
+                this._west = Double.Parse(infoGGA[4].Replace('.', ','))/100;
+                this._typeLocation = Int32.Parse(infoGGA[6]);
+                this._nbSat = Int32.Parse(infoGGA[7]);
+                this._altitude = Double.Parse(infoGGA[9].Replace('.', ','))/100;
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public double North { get => _north; set => _north = value; }
+        public double West { get => _west; set => _west = value; }
+        public double Altitude { get => _altitude; set => _altitude = value; }
+
+        public void setDateFromGGA(string laDate)
+        {
+
+            // Extraire l'heure, les minutes et les secondes de la chaîne
+            int heure = int.Parse(laDate.Substring(0, 2));
+            int minutes = int.Parse(laDate.Substring(2, 2));
+            int secondes = int.Parse(laDate.Substring(4, 2));
+
+            // Initialiser un objet DateTime avec la date actuelle et l'heure spécifiée
+            this._date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, heure, minutes, secondes);
+        }
+    }
+}
