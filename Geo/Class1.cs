549e2d803fb33e1a245112eec530411d895c3925
XIMUNMENDIBURU <152402144+XIMUNMENDIBURU@users.noreply.github.com>
2025-10-29 16:09:58 +0100

    Add: Class trace

--- Geo/Class1.cs
+++ Geo/Class1.cs
@@ -1,30 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
 
 namespace Geo
 {
-    internal class points
+    internal class Point
     {
-        public string id;
-        public double longitude;
-        public double latitude;
-        public double altitude;
-        public string time;
+        public string Id { get; set; }
+        public double Longitude { get; private set; } // en degrés
+        public double Latitude { get; private set; }  // en degrés
+        public double Altitude { get; private set; }  // en mètres
+        public string Time { get; private set; }
+
         private double x;
         private double y;
         private double z;
 
-        private const double a = 6378137.0; 
-        private const double e2 = 6.69437999014e-3; 
-        public points(double lg, double lt, double alt, string time)
+        // Constantes WGS84
+        private const double a = 6378137.0;             // demi-grand axe
+        private const double e2 = 6.69437999014e-3;     // excentricité²
+
+        public Point(double longitude, double latitude, double altitude, string time, string id = "")
         {
-            this.longitude = lg;
-            this.latitude = lt;
-            this.altitude = alt;
-            this.time = time;
+            this.Id = id;
+            this.Longitude = longitude;
+            this.Latitude = latitude;
+            this.Altitude = altitude;
+            this.Time = time;
+
             ConvertToCartesian();
         }
 
@@ -40,20 +44,63 @@ namespace Geo
             this.z = (N * (1 - e2) + Altitude) * Math.Sin(latRad);
         }
 
-        public double getX()
-        {
-            return this.x;
-        }
+        public double GetX() => x;
+        public double GetY() => y;
+        public double GetZ() => z;
 
-        public double getY()
-        {
-            return this.y;
-        }
-
-        public double getZ()
-        {
-            return this.z;
-        }
-        public void 
     }
+
+    internal class Trace
+    {
+        private string filePath = @"Geo\bin\Debug\"; 
+        public string Id { get; private set; }
+        public List<Point> PointList { get; private set; } = new List<Point>();
+
+        public Trace(string filename)
+        {
+            this.Id = filename;
+            this.filePath = Path.Combine(Directory.GetCurrentDirectory(), filename);
+        }
+
+        public void AddPoint(Point currentPos)
+        {
+            PointList.Add(currentPos);
+        }
+
+        public void Save()
+        {
+            if (PointList.Count == 0)
+            {
+                Console.WriteLine("Aucun point à enregistrer.");
+                return;
+            }
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
+                {
+                    // En-tête CSV
+                    sw.WriteLine("Longitude,Latitude,Altitude,Time");
+
+                    // Données
+                    foreach (var p in PointList)
+                    {
+                        sw.WriteLine(
+                        $"{p.Longitude.ToString("F7", System.Globalization.CultureInfo.InvariantCulture)}," +
+                        $"{p.Latitude.ToString("F7", System.Globalization.CultureInfo.InvariantCulture)}," +
+                        $"{p.Altitude.ToString("F4", System.Globalization.CultureInfo.InvariantCulture)}," +
+                        $"{p.Time}");
+                    }
+                }
+
+                Console.WriteLine($"Trace enregistrée avec succès dans : {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur lors de l'enregistrement : {ex.Message}");
+            }
+        }
+    }
+}
+
 }
